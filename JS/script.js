6916e5c68be897844a0d432f32d7b2be160758e4
TottySnowman <paul.wortmann@gibmit.ch>
2021-04-07 22:15:48 +0200

    Added Animations Added Weeknr and Year Added Week reset on Class Change Added Error Message Added Vaction Message

--- JS/script.js
+++ JS/script.js
@@ -1,99 +1,128 @@
 var url = "http://sandbox.gibm.ch/berufe.php";
-        moment.locale("de");
-        let iCWeek = moment().week();
-        let iProfessionID, iClassID;
+moment.locale("de");
+let iCWeek = moment().week();
+let iYear = moment().year();
+let iProfessionID, iClassID;
+let fMessage;
 
-        $( document ).ready(function()
-        {
-            setSettings();
-            $("#berufe").append($("<option>Beruf auswählen</option>"));
-        $.getJSON(url)
-            .done(function (data) {
+$(document).ready(function () {
+    setSettings();
+    $.getJSON(url)
+        .done(function (data) {
+            $.each(data, function () {
+                $("#berufe").append($("<option></option>").val(this['beruf_id']).html(this['beruf_name']));
+            });
+
+            if (iProfessionID != "null" && iClassID != "null") {
+                $('#berufe').val(iProfessionID).change();
+                setWeek(0);
+            }
+            else {
+                $("#berufe").append($("<option selected>Beruf auswählen</option>"));
+                $("#class").hide();
+                $("#lblClass").hide();
+                $("#btnRow").hide();
+            }
+        })
+
+        .fail(function () {
+            fMessage = "Berufe können nicht gelanden werden!";
+        })
+});
+
+function getKlasse() {
+    localStorage.setItem("gibm_ProfessionID", $("#berufe").val());
+    loadClasses($("#berufe").val());
+}
+
+function setStundenplan() {
+    localStorage.setItem("gibm_ClassID", $("#class").val());
+    iCWeek = moment().week();
+    setWeek(0);
+    getStundenplan($("#class").val());
+}
+
+function getStundenplan(classID) {
+    var url3 = "http://sandbox.gibm.ch/tafel.php?klasse_id=" + classID + "&woche=" + iCWeek + "-" +iYear;
+    var table = "";
+
+    $.getJSON(url3)
+        .done(function (data) {
+            if (data.length > 2) {
+                table += "<table class='table'><th scope='col'>Datum</th><th scope='col'>Wochentag</th><th scope='col'>Von-Bis</th><th scope='col'>Fach</th><th scope='col'>Lehrer</th><th scope='col'>Zimmer</th>";
                 $.each(data, function () {
-                    $("#berufe").append($("<option></option>").val(this['beruf_id']).html(this['beruf_name']));
+                    table += "<tr scope='row'>"
+                    table += "<td>" + moment(this['tafel_datum']).format("DD-MM-YYYY") + "</td>";
+                    table += "<td>" + moment(this['tafel_datum']).format('dddd') + "</td>";
+                    table += "<td>" + "Von " + this['tafel_von'] + " bis " + this['tafel_bis'] + "</td>";
+                    table += "<td>" + this['tafel_longfach'] + "</td>";
+                    table += "<td>" + this['tafel_lehrer'] + "</td>";
+                    table += "<td>" + this['tafel_raum'] + "</td>";
+                    table += "</tr>";
                 });
-                if(iProfessionID != "null")
-                {
+                table += "</table>";
+            }
 
-                    $('#berufe').val(iProfessionID).change();
-                }
-            })
+            else {
+                table += "Ferien";
+            }
+            $("#stundenplanOutput").empty().append(table).fadeIn("2");
+        })
 
-            .fail(function () {
-                console.log("Fail");
-            })
-            
-        });
+        .fail(function () {
+            fMessage = "Stundenplan kann nicht gelanden werden!";
+            displayError();
+        })
+}
 
-        function getKlasse() 
-        {
-            localStorage.setItem("gibm_ProfessionID", $("#berufe").val());
-            loadClasses($("#berufe").val());
-            $('#class').val(iClassID).change();
-        }
+function setWeek(count) {
+    $("#stundenplanOutput").fadeOut("1", function () {
+        iCWeek = iCWeek + count;
+        setSettings();
+        getStundenplan(iClassID);
+        // var mo = moment();
+        // var test = moment().day("Montag").week(iCWeek);
+        // var test2 = moment().week(iCWeek + 1).day('Sonntag');
+        // console.log(moment(test).format("YYYY-MM-DD"), test2.toString());
 
-        function setStundenplan()
-        {
-            localStorage.setItem("gibm_ClassID", $("#class").val());
-            getStundenplan($("#class").val());
-        }
+        $("#weekDate").empty().append(iCWeek + "-" + iYear);
+    });
 
-        function getStundenplan(classID) {
-            var url3 = "http://sandbox.gibm.ch/tafel.php?klasse_id=" + classID + "&woche=" + iCWeek + "-2021";
-            var table = "";
-            $.getJSON(url3)
-                .done(function (data) {
-                    table += "<table class='table'><th scope='col'>Datum</th><th scope='col'>Wochentag</th><th scope='col'>Von-Bis</th><th scope='col'>Fach</th><th scope='col'>Lehrer</th><th scope='col'>Zimmer</th>";
-                    $.each(data, function () {
-                        var dt = moment(this['tafel_datum'], "YYYY-MM-DD HH:mm:ss")
-                        table += "<tr scope='row'>"
-                        table += "<td>" + this['tafel_datum'] + "</td>";
-                        table += "<td>" + dt.format('dddd') + "</td>";
-                        table += "<td>" + "Von " + this['tafel_von'] + " bis " + this['tafel_bis'] + "</td>";
-                        table += "<td>" + this['tafel_longfach'] + "</td>";
-                        table += "<td>" + this['tafel_lehrer'] + "</td>";
-                        table += "<td>" + this['tafel_raum'] + "</td>";
-                        table += "</tr>";
-                    });
-                    table += "</table>";
-                    $("#stundenplanOutput").empty().append(table);
-                })
+}
 
-                .fail(function () {
-                    console.log("Fail");
-                })
-        }
+function setSettings() {
+    iProfessionID = localStorage.getItem("gibm_ProfessionID");
+    iClassID = localStorage.getItem("gibm_ClassID");
+}
 
-        function setWeek(count)
-        {
-            iCWeek = iCWeek + count;
-            setSettings();
-            getStundenplan(iClassID);
-        }
+function loadClasses(beruf_id) {
+    $("#class").empty().append('<option value="0" selected="selected">Klasse auswählen</option>');
+    var url2 = "http://sandbox.gibm.ch/klassen.php?beruf_id=" + beruf_id;
+    $.getJSON(url2)
+        .done(function (data) {
+            $.each(data, function () {
+                $("#class").append($("<option></option>").val(this['klasse_id']).html(this['klasse_longname']));
+            });
 
-        function setSettings()
-        {
-            iProfessionID = localStorage.getItem("gibm_ProfessionID");
-            iClassID = localStorage.getItem("gibm_ClassID");
-        }
+            if (iClassID != "null") {
+                $('#class').val(iClassID).change();
+            }
 
-        function loadClasses(beruf_id)
-        {
-            $("#class").empty().append('<option value="0" selected="selected">Klasse auswählen</option>');
-            var url2 = "http://sandbox.gibm.ch/klassen.php?beruf_id=" + beruf_id;
-            $.getJSON(url2)
-                .done(function (data) {
-                    $.each(data, function () {
-                        $("#class").append($("<option></option>").val(this['klasse_id']).html(this['klasse_longname']));
-                    });
-                    
-                    if(iClassID != "null")
-                    {
-                        $('#class').val(iClassID).change();
-                    }
-                })
+            $("#stundenplanOutput").empty();
+            $("#class").focus();
+            $("#lblClass").show();
+            $("#btnRow").show();
+            $("#class").show("2000");
+        })
 
-                .fail(function () {
-                    console.log("Fail");
-                })
-        }
+        .fail(function () {
+            fMessage = "Klassen können nicht gelanden werden!";
+        })
+}
+
+function displayError()
+{
+    $("#lblErrorMessage").empty().append(fMessage);
+    var myModal = new bootstrap.Modal(document.getElementById('modal'));
+    myModal.show();
+}
